a5946aad424df83bc1af53882028a80845e42f0c
Lab User <LabUser@labs.salina.k-state.edu>
2018-10-30 10:00:41 -0500

    Staged for deployment

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -7,10 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+var compression = require('compression');
+var helmet = require('helmet');
 
+// Create the express application object
 var app = express();
 
+
 // view engine setup
+app.use(helmet());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -18,6 +23,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
