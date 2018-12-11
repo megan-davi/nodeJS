faf3499c3e7df06e10de6e989e62f6cc089e052a
Megan Brown <mdavi@megandavi.tech>
2018-12-10 22:43:56 -0600

    Deleted twist-old

--- twist-old/models/session.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose')
-var Schema = mongoose.Schema;
-
-const sessionSchema = new Schema({
-    Time: {type: Date},
-    });
-
-//Export model
-module.exports = mongoose.model ('session', sessionSchema);
